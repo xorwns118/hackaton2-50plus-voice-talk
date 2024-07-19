d60b8f26fa627037c1bbb7e625a728e3a60e46c8
Tigris <kyrae604@naver.com>
2024-07-19 15:36:17 +0900

    ADD: initial setting for hackaton project

---
+++ src/main/java/com/example/hackaton250plusvoicetalk/Hackaton250plusVoiceTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackaton250plusvoicetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton250plusVoiceTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackaton250plusVoiceTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackaton250plusvoicetalk/Hackaton250plusVoiceTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackaton250plusvoicetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton250plusVoiceTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
