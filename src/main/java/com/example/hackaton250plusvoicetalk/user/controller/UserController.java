f9dac0d78fe76abc619be5853aa9d0934d5bcb7b
Tigris <kyrae604@naver.com>
2024-07-22 12:33:34 +0900

    FEAT: login and check authority by mobile number at login and join

--- src/main/java/com/example/hackaton250plusvoicetalk/user/controller/UserController.java
+++ src/main/java/com/example/hackaton250plusvoicetalk/user/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.hackaton250plusvoicetalk.user.controller;
 
+import com.example.hackaton250plusvoicetalk.constants.Authority;
 import com.example.hackaton250plusvoicetalk.user.service.UserService;
 import com.example.hackaton250plusvoicetalk.user.web.model.Api;
 import com.example.hackaton250plusvoicetalk.user.web.model.User;
+import com.example.hackaton250plusvoicetalk.user.web.vo.UserLoginRequest;
 import com.example.hackaton250plusvoicetalk.user.web.vo.UserRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +24,39 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@SessionAttributes("users")
 public class UserController {
     private final UserService userService;
 
 
     // ========== GET ==========
+
+    /**
+     * Index string.
+     *
+     * @return main page
+     */
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    /**
+     * Logout string.
+     *
+     * @param httpServletRequest get httpServletRequest for session
+     * @return main page
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest httpServletRequest){
+        HttpSession session = httpServletRequest.getSession();
+        if(session != null){
+            session.invalidate();
+        }
+        return "index";
+    }
+
+
     /**
      * Login string.
      *
@@ -45,22 +78,45 @@ public class UserController {
     }
 
 
-//    // ========== POST ==========
-//    @PostMapping("/login")  // login->login.html logout->logout.html
-//    public Api<UserRequest> login(@RequestBody ){
-//
-//    }
+    /**
+     * Login user api.
+     *
+     * @param userLoginRequest   the user login request
+     * @param httpServletRequest the http servlet request
+     * @return the api
+     */
+// ========== POST ==========
+    @PostMapping("/login")  // login->login.html logout->logout.html
+    public Api<UserLoginRequest> loginUser(@RequestBody @Valid Api<UserLoginRequest> userLoginRequest, HttpServletRequest httpServletRequest){
+        try{
+            // check Authority by using mobile number
+            if(userLoginRequest.getData().getMobileNumber().equals("010-0000-0000")){
+                userLoginRequest.getData().setAuthority(Authority.ROLE_ADMIN);
+            }else{
+                userLoginRequest.getData().setAuthority(Authority.ROLE_USER);
+            }
+
+            Api<UserLoginRequest> response = userService.login(userLoginRequest, httpServletRequest);
+            return response;
+        }catch (IllegalArgumentException e){
+            log.error(e.getMessage());
+
+            return Api.<UserLoginRequest>builder()
+                    .resultCode(String.valueOf(HttpStatus.NOT_FOUND))
+                    .resultMessage(e.getMessage())
+                    .build();
+        }
+    }
 
     /**
      * Join user response entity.
      *
-     * @param userRequest 유저로부터 받은 정보
-     * @return DB에 저장된 유저 정보
+     * @param userRequest information got from user
+     * @return user info stored at DB
      */
     @PostMapping("/join")
     public Api<UserRequest> joinUser(@RequestBody @Valid Api<UserRequest> userRequest) {
-        Api<UserRequest> response = userService.join(userRequest);
-        return response;
+        return userService.join(userRequest);
     }
 
 
--- src/main/java/com/example/hackaton250plusvoicetalk/user/service/UserService.java
+++ src/main/java/com/example/hackaton250plusvoicetalk/user/service/UserService.java
@@ -2,36 +2,55 @@ package com.example.hackaton250plusvoicetalk.user.service;
 
 
 import com.example.hackaton250plusvoicetalk.constants.Authority;
+import com.example.hackaton250plusvoicetalk.constants.SessionConst;
 import com.example.hackaton250plusvoicetalk.user.persist.UserRepository;
 import com.example.hackaton250plusvoicetalk.user.persist.entity.UserEntity;
 import com.example.hackaton250plusvoicetalk.user.web.model.Api;
+import com.example.hackaton250plusvoicetalk.user.web.vo.UserLoginRequest;
 import com.example.hackaton250plusvoicetalk.user.web.vo.UserRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Optional;
 
+/**
+ * The type User service.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final UserSecurityService userSecurityService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
+    /**
+     * Join api.
+     *
+     * @param userRequest user join request
+     * @return user join data and HttpStatus info
+     */
     @Transactional
     public Api<UserRequest> join(@Valid Api<UserRequest> userRequest) {
         var body = userRequest.getData();
+
         String encryptedPassword = bCryptPasswordEncoder.encode(body.getPassword());
         body.setPassword(encryptedPassword);
-        body.setAuthority(Authority.ROLE_USER);
+        if(body.getMobileNumber().equals("010-0000-0000")){ // set authority as admin
+            body.setAuthority(Authority.ROLE_ADMIN);
+        }else{
+            body.setAuthority(Authority.ROLE_USER);
+        }
 
         UserEntity userEntity = UserEntity.builder()
                 .username(body.getUsername())
@@ -54,16 +73,55 @@ public class UserService {
         return response;
     }
 
-    public Api<UserRequest> login(Api<UserRequest> userRequest) {
-        var body = userRequest.getData();
-        UserDetails userDetails = userSecurityService.loadUserByMobileNumber(body.getMobileNumber());
+    /**
+     * Login api.
+     *
+     * @param userLoginRequest   the user login request
+     * @param httpServletRequest the http servlet request for login session
+     * @return login data and HttpStatus info
+     */
+    public Api<UserLoginRequest> login(Api<UserLoginRequest> userLoginRequest, HttpServletRequest httpServletRequest) {
+        var body = userLoginRequest.getData();
 
-        Api<UserRequest> response = Api.<UserRequest>builder()
-                .resultCode(String.valueOf(HttpStatus.OK.value()))
-                .resultMessage(HttpStatus.OK.getReasonPhrase())
-                .data(body)
-                .build();
+        String loginMobileNumber = body.getMobileNumber();
+        String loginPassword = body.getPassword();
 
+        UserEntity userInfoEntity = getUserDetailsByMobileNumber(loginMobileNumber);
+        Api<UserLoginRequest> response;
+
+        if(userInfoEntity != null && bCryptPasswordEncoder.matches(loginPassword, userInfoEntity.getPassword())){   // login success
+            HttpSession session = httpServletRequest.getSession();
+            // TODO: Authority에 따른 리다이렉트 페이지 다르게 연결
+            if(body.getAuthority().equals(Authority.ROLE_USER)){
+                session.setAttribute(SessionConst.LOGIN_USER, loginMobileNumber);
+            }else{
+                session.setAttribute(SessionConst.LOGIN_ADMIN, loginMobileNumber);
+            }
+            response = Api.<UserLoginRequest>builder()
+                    .resultCode(String.valueOf(HttpStatus.OK.value()))
+                    .resultMessage(HttpStatus.OK.getReasonPhrase())
+                    .data(body)
+                    .build();
+        }else{                                                                                  // login failure
+            log.info("wrong password");
+            response = Api.<UserLoginRequest>builder()
+                    .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                    .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                    .data(body)
+                    .build();
+        }
         return response;
+    }
+
+
+    /**
+     * Gets user details by mobile number.
+     *
+     * @param mobileNumber the mobile number
+     * @return the user details found by mobile number
+     */
+    public UserEntity getUserDetailsByMobileNumber(String mobileNumber) {
+        return userRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with mobile number: " + mobileNumber));
     }
 }
